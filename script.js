5b1583270fe5789c4a37ca828b96ee05f37d8439
Vivesh kumar <vivesh103@gmail.com>
2024-12-28 16:06:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,6 +10,6 @@ function shortcut(s1, s2) {
 }
 
 // Do not change the code below.
-// const s1 = prompt("Enter s1:");
-// const s2 = prompt("Enter s2:");
-// alert(shortcut(s1, s2));
+const s1 = prompt("Enter s1:");
+const s2 = prompt("Enter s2:");
+alert(shortcut(s1, s2));
